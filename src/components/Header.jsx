ec7397219a63aeff047d6342c79965073069c970
VladG00dman <68503186+VladSunny@users.noreply.github.com>
2025-03-19 16:25:00 +0300

    upd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
-      <div class="flex bg-cyan-700 justify-center items-center w-full px-4">
-        <div class="card flex justify-center items-center w-5xl my-5">
-            <h1 class="text-center">Truth Or Dare</h1>
-        </div>
+      <div class="flex justify-center items-center w-full px-4">
+        <Link to="/">
+          <div class="card flex justify-center items-center w-5xl my-5">
+              <h1 class="text-center">Truth Or Dare</h1>
+          </div>
+        </Link>
       </div>
     </>
   )
