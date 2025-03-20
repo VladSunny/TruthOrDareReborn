4807f91c7dfb429ef64f48829852e15eb5e91963
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2025-03-20 17:25:03 +0300

    upd

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -5,14 +5,14 @@ function Studio() {
   return (
     <>
       <div className="flex justify-center items-center h-dvh flex-col">
-        <h1 className='absolute top-30 md:top-40 text-3xl md:text-6xl xl:text-8xl font-bold text-secondary bg-base-200 p-4 rounded-box'>
+        <h1 className='absolute top-22 md:top-40 text-3xl md:text-6xl xl:text-8xl font-bold text-secondary bg-base-200 p-4 rounded-box'>
           Studio
         </h1>
 
-        <fieldset class="fieldset bg-base-200 border border-base-300 mt-20 p-4 rounded-box w-2/3 md:w-4/7 lg:w-3/7">
+        <fieldset class="fieldset bg-base-200 border border-base-300 mt-30 p-4 rounded-box w-3/4 md:w-4/7 lg:w-3/7">
           <legend class="fieldset-legend text-xl md:text-3xl text-primary">Your Idea</legend>
           
-          <textarea class="textarea h-50 max-h-80 w-full md:text-2xl" placeholder="Bio"></textarea>
+          <textarea class="textarea h-30 max-h-50 w-full md:text-2xl" placeholder="Idea"></textarea>
           
           <span className="label text-xl md:text-2xl mt-5 text-secondary">Type</span>
           <select className='text-xl outline-primary text-secondary'>
