fc607dc45994f348765ffc601fb7d935f0d9354d
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2025-03-22 17:17:06 +0300

    upd

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -3,6 +3,7 @@ import supabase from '../supabase-client';
 
 function Studio() {
   const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
   const [ideaText, setIdeaText] = useState('');
   const [selectedOption, setSelectedOption] = useState('truth');
 
@@ -14,12 +15,12 @@ function Studio() {
 
     console.log('Form Submission:', idea);
 
-    const {data, error} = await supabase.from('Ideas').insert([idea]).single();
+    const { data, error } = await supabase.from('Ideas').insert([idea]).single();
 
     if (error) {
       console.error('Error inserting data:', error);
-    }
-    else {
+      openErrorAlert();
+    } else {
       openSuccessAlert();
       setIdeaText('');
     }
@@ -30,53 +31,60 @@ function Studio() {
     setTimeout(() => {
       setShowSuccessAlert(false);
     }, 3000);
-  }
+  };
+
+  const openErrorAlert = () => {
+    setShowErrorAlert(true);
+    setTimeout(() => {
+      setShowErrorAlert(false);
+    }, 3000);
+  };
 
   return (
     <>
       <div className="flex items-center h-dvh flex-col">
-        <h1 className='absolute top-22 md:top-40 text-3xl md:text-6xl xl:text-8xl font-bold text-secondary bg-base-200 p-4 rounded-box'>
+        <h1 className="absolute top-22 md:top-40 text-3xl md:text-6xl xl:text-8xl font-bold text-secondary bg-base-200 p-4 rounded-box">
           Studio
         </h1>
 
-        <div className='flex flex-col items-center justify-around absolute top-40 md:top-75 w-screen'>
+        <div className="flex flex-col items-center justify-around absolute top-40 md:top-75 w-screen">
           <fieldset className="fieldset bg-base-200 border border-base-300 p-4 rounded-box w-3/4 shadow-2xl hover:shadow-secondary transition-all duration-500 md:w-4/7 lg:w-3/7">
             <legend className="fieldset-legend text-xl md:text-3xl text-primary">Your Idea</legend>
             
-            <textarea 
-              className="textarea h-30 max-h-50 w-full md:text-2xl text-accent-content" 
+            <textarea
+              className="textarea h-30 max-h-50 w-full md:text-2xl text-accent-content"
               placeholder="Idea"
               value={ideaText}
               onChange={(e) => setIdeaText(e.target.value)}
             />
             
-            <div className='flex flex-col space-y-3 mt-5'>
-              <div className='flex flex-row space-x-3 items-center'>
-                <input 
-                  type="radio" 
-                  name="radio-4" 
-                  className="radio radio-primary" 
+            <div className="flex flex-col space-y-3 mt-5">
+              <div className="flex flex-row space-x-3 items-center">
+                <input
+                  type="radio"
+                  name="radio-4"
+                  className="radio radio-primary"
                   checked={selectedOption === 'truth'}
                   onChange={() => setSelectedOption('truth')}
                 />
-                <p className='text-xl md:text-2xl text-primary'>Truth</p>
+                <p className="text-xl md:text-2xl text-primary">Truth</p>
               </div>
 
-              <div className='flex flex-row space-x-3 items-center'>
-                <input 
-                  type="radio" 
-                  name="radio-4" 
+              <div className="flex flex-row space-x-3 items-center">
+                <input
+                  type="radio"
+                  name="radio-4"
                   className="radio radio-secondary"
                   checked={selectedOption === 'dare'}
                   onChange={() => setSelectedOption('dare')}
                 />
-                <p className='text-xl md:text-2xl text-secondary'>Dare</p>
+                <p className="text-xl md:text-2xl text-secondary">Dare</p>
               </div>
             </div>
           </fieldset>
 
-          <button 
-            className='btn btn-lg btn-success w-1/3 lg:w-1/5 mt-10 hover:btn-xl transition-all duration-500 subpixel-antialiased' 
+          <button
+            className="btn btn-lg btn-success w-1/3 lg:w-1/5 mt-10 hover:btn-xl transition-all duration-500 subpixel-antialiased"
             onClick={() => {
               if (ideaText) {
                 document.getElementById('my_modal_1').showModal();
@@ -90,10 +98,9 @@ function Studio() {
 
       <dialog id="my_modal_1" className="modal">
         <div className="modal-box">
-          {/* <h3 className="font-bold text-lg">Hello!</h3> */}
           <p className="py-4">Are you sure?</p>
           <div className="modal-action">
-            <form method="dialog" className='flex flex-row space-x-3'>
+            <form method="dialog" className="flex flex-row space-x-3">
               <button className="btn">Close</button>
               <button className="btn" onClick={handleSubmit}>Send</button>
             </form>
@@ -101,17 +108,51 @@ function Studio() {
         </div>
       </dialog>
 
-      <div className='flex justify-center'>
-        <div 
-          role="alert" 
+      <div className="flex justify-center">
+        <div
+          role="alert"
           className={`absolute top-1/5 md:top-1/3 alert alert-success alert-soft transition-opacity duration-500 ${
             showSuccessAlert ? 'opacity-100' : 'opacity-0'
           }`}
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
           </svg>
-          <span className='text-xl'>Sended!</span>
+          <span className="text-xl">Sended!</span>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div
+          role="alert"
+          className={`absolute top-1/5 md:top-1/3 alert alert-error alert-soft transition-opacity duration-500 ${
+            showErrorAlert ? 'opacity-100' : 'opacity-0'
+          }`}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 shrink-0 stroke-current"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          <span className="text-xl">Error occurred!</span>
         </div>
       </div>
     </>
