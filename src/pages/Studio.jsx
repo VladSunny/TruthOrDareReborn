a76c42ad1025645e52a0f8faaf4b55eb1f2038cd
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2025-03-22 08:28:39 +0300

    upd

--- src/pages/Studio.jsx
+++ src/pages/Studio.jsx
@@ -1,7 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import supabase from '../supabase-client';
 
 function Studio() {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleSubmit = () => {
+    setShowAlert(true);
+
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 3000);
+  };
+
   return (
     <>
       <div className="flex justify-center items-center h-dvh flex-col">
@@ -29,9 +39,23 @@ function Studio() {
           </div>
         </fieldset>
 
-        <button className='btn btn-lg btn-success w-1/3 lg:w-1/5 mt-10'>
+        <button className='btn btn-lg btn-success w-1/3 lg:w-1/5 mt-10' onClick={handleSubmit}>
             Submit
         </button>
+      </div>
+
+      <div className='flex justify-center'>
+        <div 
+            role="alert" 
+            className={`absolute top-1/5 md:top-1/3 alert alert-success alert-soft transition-opacity duration-500 ${
+              showAlert ? 'opacity-100' : 'opacity-0'
+            }`}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 shrink-0 stroke-current" fill="none" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <span className='text-xl'>Sended!</span>
+        </div>
       </div>
     </>
   );
