1bed4eb78ddbd457e8c44c4646245a0ff57939ae
Vlad Sunny <68503186+VladSunny@users.noreply.github.com>
2025-04-04 08:36:20 +0300

    upd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import supabase from '../supabase-client';
+import NavigationButton from '../components/NavigationButton';
 
 function Home() {
   const [currentCard, setCurrentCard] = useState("Choose");
@@ -74,7 +75,7 @@ function Home() {
   return (
     <>
       <div className="flex justify-center items-center h-dvh flex-col">
-        {session?.user && <p className="text-2xl md:text-3xl mb-5">Hello, {session?.user?.user_metadata?.full_name}</p>}
+        {/* {session?.user && <p className="text-2xl md:text-3xl mb-5">Hello, {session?.user?.user_metadata?.full_name}</p>} */}
         <div className="card z-0 bg-base-300 w-5/6 md:w-7/12 lg:w-3/7 h-1/2 shadow-2xl hover:shadow-primary transition-all duration-500">
           <div className="card-body">
             <h2 className="card-title">
